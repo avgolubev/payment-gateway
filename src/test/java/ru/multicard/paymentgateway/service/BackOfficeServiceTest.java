1bed7ed4a4fee96e98dd84ea26da72ab51664772
AGolubev <AGolubev@poidem.ru>
2019-04-05 10:45:41 +0500

    change test comments

--- src/test/java/ru/multicard/paymentgateway/service/BackOfficeServiceTest.java
+++ src/test/java/ru/multicard/paymentgateway/service/BackOfficeServiceTest.java
@@ -18,6 +18,7 @@ import static ru.multicard.paymentgateway.service.OperationError.*;
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 @NoArgsConstructor
+@DisplayName("Test BackOfficeService functionality.")
 public class BackOfficeServiceTest {
 
   @Autowired
@@ -37,15 +38,15 @@ public class BackOfficeServiceTest {
   public void checkAccountNotAllParametersFilled() {
     final CheckAccountRequest request = new CheckAccountRequest("1", "", "123");
     assertEquals(ABSENT_PARAMETER.getCode(), backOfficeService.checkAccount(request).getRetval(),
-      "A result with error ABSENT_PARAMETER is returned, if at least one query parameter is missing.");
+      "Error should be ABSENT_PARAMETER when at least one query parameter is missing.");
 
     request.setSign("xx");
     assertEquals(MD5_ERROR.getCode(), backOfficeService.checkAccount(request).getRetval(),
-      "A result with error MD5_ERROR is returned, if parameter sign has invalid md5 hash.");
+      "Error should be MD5_ERROR when sign parameter is invalid md5 hash.");
 
     request.setSign(md5Hash(request.getNumber()));
     assertEquals(NO_ERROR.getCode(), backOfficeService.checkAccount(request).getRetval(),
-      "A result with error NO_ERROR is returned, if nothing errors in operation.");
+      "Error should be NO_ERROR  when nothing errors in operation.");
   }
 
   @Test
@@ -54,23 +55,23 @@ public class BackOfficeServiceTest {
     final ChargeAccountRequest request = new ChargeAccountRequest("0", "", "123", "12,00",
       "xxxx", ".13.2019");
     assertEquals(ABSENT_PARAMETER.getCode(), backOfficeService.chargeAccount(request).getRetval(),
-      "A result with error ABSENT_PARAMETER is returned, if at least one query parameter is missing.");
+      "Error should be ABSENT_PARAMETER when at least one query parameter is missing.");
 
     request.setSign("xxx");
     assertEquals(MD5_ERROR.getCode(), backOfficeService.chargeAccount(request).getRetval(),
-      "A result with error MD5_ERROR is returned, if parameter sign has invalid md5 hash.");
+      "Error should be MD5_ERROR when sign parameter is invalid md5 hash.");
 
     request.setSign(md5Hash(request.getNumber()));
     assertEquals(INVALID_AMOUNT_FORMAT.getCode(), backOfficeService.chargeAccount(request).getRetval(),
-      "A result with error INVALID_AMOUNT_FORMAT is returned, if parameter amount has invalid format.");
+      "Error should be INVALID_AMOUNT_FORMAT when parameter amount has invalid format.");
 
     request.setAmount("1200.78");
     assertEquals(INVALID_PAYMENT_DATE_FORMAT.getCode(), backOfficeService.chargeAccount(request).getRetval(),
-      "A result with error INVALID_PAYMENT_DATE_FORMAT is returned, if parameter paymentCreate has invalid format.");
+      "Error should be INVALID_PAYMENT_DATE_FORMAT when paymentCreate parameter has invalid format.");
 
     request.setPaymentCreate("12.03.2019 12:34:00");
     assertEquals(NO_ERROR.getCode(), backOfficeService.chargeAccount(request).getRetval(),
-      "A result with error NO_ERROR is returned, if nothing errors in operation.");
+      "Error should be NO_ERROR when nothing errors in operation.");
 
   }
 
