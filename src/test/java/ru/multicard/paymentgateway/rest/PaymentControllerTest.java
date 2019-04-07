eb07d5c334d751a35f8fd51c58ffcb96e4bcc2bb
Andrei Golubev <andrei.v.g@icloud.com>
2019-04-08 01:02:57 +0500

    add tests

---
+++ src/test/java/ru/multicard/paymentgateway/rest/PaymentControllerTest.java
@@ -0,0 +1,29 @@
+package ru.multicard.paymentgateway.rest;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class PaymentControllerTest {
+
+  @BeforeEach
+  void setUp() {
+  }
+
+  @Test
+  void getCheckAccount() {
+  }
+
+  @Test
+  void postCheckAccount() {
+  }
+
+  @Test
+  void getChargeAccount() {
+  }
+
+  @Test
+  void postChargeAccount() {
+  }
+}
