6ec8e324768c46403330f1b927b16ad1f69df2fd
Andrei Golubev <andrei.v.g@icloud.com>
2019-04-12 16:24:14 +0700

    Update ServletInitializer.java

--- src/main/java/ru/multicard/paymentgateway/ServletInitializer.java
+++ src/main/java/ru/multicard/paymentgateway/ServletInitializer.java
@@ -7,7 +7,7 @@ import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 
 @NoArgsConstructor
 public class ServletInitializer extends SpringBootServletInitializer {
-
+  
 	@Override
 	protected SpringApplicationBuilder configure(final SpringApplicationBuilder application) {
 		return application.sources(PaymentGatewayApplication.class);
