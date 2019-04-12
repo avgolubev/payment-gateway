e15b038bea89345deaf403396731ff3e3777bfdc
Andrei Golubev <andrei.v.g@icloud.com>
2019-04-12 15:02:25 +0700

    Update ChargeAccountRequest.java

--- src/main/java/ru/multicard/paymentgateway/dto/ChargeAccountRequest.java
+++ src/main/java/ru/multicard/paymentgateway/dto/ChargeAccountRequest.java
@@ -9,7 +9,6 @@ import lombok.NoArgsConstructor;
 
 import javax.xml.bind.annotation.XmlAccessType;
 import javax.xml.bind.annotation.XmlAccessorType;
-import javax.xml.bind.annotation.XmlElement;
 import javax.xml.bind.annotation.XmlRootElement;
 import java.util.regex.Pattern;
 
